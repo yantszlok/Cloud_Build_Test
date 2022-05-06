be8c2d3e1d1bfa2984e2fcca8c9c291507636152
tszlok yan <tszlokyan@tszloknoMacBook-Air.local>
2022-05-06 14:59:56 +0900

    test15

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
- exports.cloudbuildtest = (req, res) => {
+ exports.cloudBuildTest = (req, res) => {
     let message = req.query.message || req.body.message || 'Cloud Build Test from yantszlok repository test12345678';
     res.status(200).send(message);
   };
